3a999aa56ef918731c300b866eea3fd31662a27d
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-02-03 23:16:02 -0300

    implementa a feature de fazer o led piscar 5 vezes por segundo

--- interuption.c
+++ interuption.c
@@ -1,14 +1,26 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
 
+#define red_pin 13
+#define time_sleep 100
 
+// Função que define o estado do led
+void set_led_color(uint pin, bool status) {
+    gpio_put(pin, status);
+}
 
-int main()
-{
-    stdio_init_all();
+int main() {
+    // Inicialização do pino do LED
+    gpio_init(red_pin);
+    gpio_set_dir(red_pin, GPIO_OUT);
 
+    bool led_status = false; 
+
+    // Loop principal
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        set_led_color(red_pin, led_status);
+        led_status = !led_status; // Inverte o estado do LED
+        sleep_ms(time_sleep);
     }
+
+  return 0;
 }
